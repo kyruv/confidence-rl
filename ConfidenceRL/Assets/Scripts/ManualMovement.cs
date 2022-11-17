2403ee49ccd8a58552bb2209b591aad9bf865757
kyruv <kylerruvane@gmail.com>
2022-11-16 20:28:56 -0700

    base sim environment

---
+++ ConfidenceRL/Assets/Scripts/ManualMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManualMovement : MonoBehaviour
+{
+    private CharacterController _controller;
+    private float _playerSpeed = 4.0f;
+    private float _rotationSpeed = 120;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 rotation = new Vector3(Input.GetAxisRaw("Horizontal") * _rotationSpeed * Time.deltaTime, 0, 0);
+
+        Vector3 move = new Vector3(0, -Input.GetAxisRaw("Vertical") * Time.deltaTime, 0);
+        move = transform.TransformDirection(move);
+        _controller.Move(move * _playerSpeed);
+        transform.Rotate(rotation);
+    }
+}
---
+++ ConfidenceRL/Assets/Scripts/Perception.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perception : MonoBehaviour
+{
+    private Camera frontCamera;
+    private Camera leftCamera;
+    private Camera rightCamera;
+
+    private Dictionary<string, Vector3> rayCastDirections = new Dictionary<string, Vector3>();
+    private Dictionary<string, float> tagMapping = new Dictionary<string, float>();
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rayCastDirections.Add("forward", Vector3.down);
+        rayCastDirections.Add("right", Vector3.back);
+        rayCastDirections.Add("left", Vector3.forward);
+
+        tagMapping.Add("obstacle", 0f);
+        tagMapping.Add("tennisball", 1f);
+
+        frontCamera = GameObject.Find("FrontCamera").GetComponent<Camera>();
+        leftCamera = GameObject.Find("LeftCamera").GetComponent<Camera>();
+        rightCamera = GameObject.Find("RightCamera").GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        GetPerception();
+    }
+
+    public List<float> GetPerception()
+    {
+        // Left distance, Left object, Left confidence, Forward distance, ...
+        List<float> perceptions = new List<float>();
+
+        // don't collide with the dog itself
+        int layerMask = 1 << 8;
+        layerMask = ~layerMask;
+
+        Vector3 origin = new Vector3(transform.position.x, .75f, transform.position.z);
+
+        RaycastHit hit;
+        if (Physics.Raycast(origin, transform.TransformDirection(rayCastDirections["left"]), out hit, 200f, layerMask))
+        {
+            Debug.DrawRay(origin, transform.TransformDirection(rayCastDirections["left"]) * hit.distance, Color.yellow);
+            perceptions.Add(hit.distance);
+            perceptions.Add(tagMapping[hit.transform.gameObject.tag]);
+            perceptions.Add(1f);
+        }
+
+        if (Physics.Raycast(origin, transform.TransformDirection(rayCastDirections["forward"]), out hit, 200f, layerMask))
+        {
+            Debug.DrawRay(origin, transform.TransformDirection(rayCastDirections["forward"]) * hit.distance, Color.yellow);
+            perceptions.Add(hit.distance);
+            perceptions.Add(tagMapping[hit.transform.gameObject.tag]);
+            perceptions.Add(1f);
+        }
+
+        if (Physics.Raycast(origin, transform.TransformDirection(rayCastDirections["right"]), out hit, 200f, layerMask))
+        {
+            Debug.DrawRay(origin, transform.TransformDirection(rayCastDirections["right"]) * hit.distance, Color.yellow);
+            perceptions.Add(hit.distance);
+            perceptions.Add(tagMapping[hit.transform.gameObject.tag]);
+            perceptions.Add(1f);
+        }
+
+        return perceptions;
+    }
+}
